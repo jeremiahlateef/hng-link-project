d1a04e31965d6db91d2dfbcadccdd6b399147608
unknown <lateefjeremiah@gmail.com>
2022-11-04 11:27:10 +0100

    removed the Footer and Profile and added it to a new components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import ProfileImage from "./Images/jeremiah-lateef.jpeg";
 
-import SlackLogo from "./Assets/slack.svg";
-import GitHubLogo from "./Assets/github-icon.svg";
-import ZuriLogo from "./Assets/zuri-logo.svg";
 import ShareMobile from "./Assets/share.svg";
 import ShareDesktop from "./Assets/share-button.svg";
-import IngressiveLogo from "./Assets/I4G.svg";
-import Camera from "./Assets/camera.svg";
+
 import "./App.css";
+
+import { Footer } from "./Footer";
+import { Profile } from "./Profile";
 
 const linksContent = [
   {
@@ -25,49 +23,31 @@ const linksContent = [
     linkName: "Zuri Books",
     id: "books",
     link: "http://books.zuri.team/",
-    title: "You'll fnd books about coding and designs here"
+    title: "You'll fnd books about coding and designs here",
   },
   {
     linkName: "Python Books",
     id: "btn__python",
     link: "https://books.zuri.team/python-for-beginners?ref_id=JeremiahLateef",
-    title: "Stuck in python? Purchase this book to help you understand the fundamentals of coding"
+    title:
+      "Stuck in python? Purchase this book to help you understand the fundamentals of coding",
   },
   {
     linkName: "Background Check for Coders",
     id: "pitch",
     link: "https://background.zuri.team",
-    title: "Worried about who you're hiring? Leave that to us, we'll help screen the best for your organization!"
+    title:
+      "Worried about who you're hiring? Leave that to us, we'll help screen the best for your organization!",
   },
   {
     linkName: "Design Books",
     id: "book__design",
     link: "https://books.zuri.team/design-rules",
-    title: "Download the free books to take your design skills to a higher level"
+    title:
+      "Download the free books to take your design skills to a higher level",
   },
 ];
 
-const Profile = () => {
-  return (
-    <React.Fragment>
-      <div className="image-cont">
-        <img
-          src={ProfileImage}
-          alt="Jeremiah Lateef"
-          className="profile-image"
-          id="profile__img"
-        />
-        <img src={Camera} alt="camera" className="camera-img" />
-      </div>
-      <div className="header">
-        <h1 className="title-header username">Jeremiah Lateef</h1>
-        <h2 className="slack-title" id="slack">
-          JeremiahLateef
-        </h2>
-      </div>
-    </React.Fragment>
-  );
-};
 const Links = (props) => {
   const { linkName, link, id, title } = props.links;
   return (
@@ -99,27 +79,7 @@ function App() {
         {linksContent.map((link) => {
           return <Links key={link.id} links={link} />;
         })}
-        <div className="icons-container">
-          <div className="icon-cont">
-            <img src={SlackLogo} alt="slack logo" className="logo" />
-          </div>
-          <div className="icon-cont">
-            <img src={GitHubLogo} alt="GitHub logo" className="logo" />
-          </div>
-        </div>
-        <footer className="footer">
-          <div className="zuri-cont">
-            <img src={ZuriLogo} alt="Zuri Internship Logo" className="zuri" />
-          </div>
-          <p className="footer-text">HNG Internship 9 Frontend Task</p>
-          <div className="ing-cont">
-            <img
-              src={IngressiveLogo}
-              alt="Ingressive for Good Logo"
-              className="zuri"
-            />
-          </div>
-        </footer>
+        <Footer />
       </div>
     </section>
   );
